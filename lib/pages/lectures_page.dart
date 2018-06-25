b4e612c4b9495725243c193b0ed68a9b27fcccaf
unknown <durduw.b@gmail.com>
2018-06-25 16:59:32 +0500

    Lectures Page added

--- lib/pages/lectures_page.dart
+++ lib/pages/lectures_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 
 import '../helpers/app_constants.dart';
-import '../list_items/item_offences.dart';
+import 'dart:async';
 
-class LecturesPage extends StatelessWidget {
+class LecturesPage extends StatefulWidget {
+  @override
+  _LecturesPageState createState() => _LecturesPageState();
+}
+
+class _LecturesPageState extends State<LecturesPage>
+    with SingleTickerProviderStateMixin {
   List<String> _lectureNames = new List();
+  TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 3, vsync: this);
+  }
+
   // List<Widget> lecturecards = List.generate(5, (i) => CustomCard());
   @override
   Widget build(BuildContext context) {
@@ -12,35 +27,38 @@ class LecturesPage extends StatelessWidget {
     _lectureNames.add('Lecture 2');
     _lectureNames.add('Lecture 3');
 
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        backgroundColor: Theme.of(context).backgroundColor,
-        appBar: AppBar(
-          bottom: TabBar(
-            tabs: [
-              Tab(text: ('Lectures')),
-              Tab(text: ('Downloading')),
-              Tab(text: ('Downloaded')),
-            ],
-          ),
-          title: Text('Lecture Materials'),
-        ),
-        body: TabBarView(
-          children: [
-            ListView.builder(
-                itemCount: _lectureNames.length,
-                itemBuilder: (context, index) => index == 0
-                    ? Padding(
-                        padding: const EdgeInsets.only(top: 8.0),
-                        child: CustomCard(lectureName: _lectureNames[index]),
-                      )
-                    : CustomCard(lectureName: _lectureNames[index])),
-            //Icon(Icons.directions_car),
-            Icon(Icons.directions_transit),
-            Icon(Icons.directions_bike),
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: AppBar(
+        bottom: TabBar(
+          tabs: [
+            Tab(text: ('Lectures')),
+            Tab(text: ('Downloading')),
+            Tab(text: ('Downloaded')),
           ],
+          controller: _controller,
         ),
+        title: Text('Lecture Materials'),
+      ),
+      body: TabBarView(
+        controller: _controller,
+        children: [
+          ListView.builder(
+              itemCount: _lectureNames.length,
+              itemBuilder: (context, index) => index == 0
+                  ? Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: CustomCard(
+                          lectureName: _lectureNames[index],
+                          controller: _controller),
+                    )
+                  : CustomCard(
+                      lectureName: _lectureNames[index],
+                      controller: _controller)),
+          //Icon(Icons.directions_car),
+          Icon(Icons.directions_transit),
+          Icon(Icons.directions_bike),
+        ],
       ),
     );
   }
@@ -48,8 +66,48 @@ class LecturesPage extends StatelessWidget {
 
 class CustomCard extends StatelessWidget {
   final lectureName;
+  final controller;
 
-  CustomCard({this.lectureName});
+  CustomCard({@required this.lectureName, @required this.controller});
+  Future _getPermissionToDownload(BuildContext context) async {
+    await showDialog(
+        context: context,
+        builder: (context) => SimpleDialog(
+              titlePadding: EdgeInsets.only(
+                  top: 15.0, left: 24.0, right: 24.0, bottom: 5.0),
+              contentPadding: EdgeInsets.only(bottom: 0.0),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Download'),
+                  InkWell(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(Icons.cloud_download),
+                      ),
+                      onTap: () {
+                        Navigator.pop(context);
+                        controller.animateTo(1);
+                      })
+                ],
+              ),
+              children: <Widget>[
+                CustomSimpleDialogOption(controller: controller),
+                CustomSimpleDialogOption(controller: controller),
+                CustomSimpleDialogOption(controller: controller),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: FlatButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text(
+                      'Cancel',
+                      style: TextStyle(color: accentColor),
+                    ),
+                  ),
+                )
+              ],
+            ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,7 +117,7 @@ class CustomCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(600.0),
         child: Card(
           child: InkWell(
-            onTap: () {},
+            onTap: () => _getPermissionToDownload(context),
             child: Padding(
               padding: const EdgeInsets.all(10.0),
               child: Text(
@@ -74,6 +132,39 @@ class CustomCard extends StatelessWidget {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class CustomSimpleDialogOption extends StatelessWidget {
+  CustomSimpleDialogOption({
+    Key key,
+    @required this.controller,
+  }) : super(key: key);
+
+  final controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialogOption(
+      onPressed: () {
+        Navigator.pop(context);
+        controller.animateTo(1);
+      },
+      //=> Navigator.pop(context),
+      child: Row(
+        children: <Widget>[
+          Icon(
+            Icons.file_download,
+            color: lightGreyTextColor,
+          ),
+          SizedBox(width: 10.0),
+          Text(
+            'Download',
+            style: TextStyle(color: lightGreyTextColor),
+          )
+        ],
       ),
     );
   }
