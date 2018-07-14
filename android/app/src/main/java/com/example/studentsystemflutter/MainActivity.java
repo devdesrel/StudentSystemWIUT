a4d21d3549ee6d61003988a7a5cf16ea7690bae4
rtoshmukhamedov <rtoshmukhamedov@gmail.com>
2018-07-14 15:55:07 +0500

    Android open file step 2

--- android/app/src/main/java/com/example/studentsystemflutter/MainActivity.java
+++ android/app/src/main/java/com/example/studentsystemflutter/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.studentsystemflutter;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.MimeTypeMap;
+
+import java.io.File;
+import java.util.Map;
 
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
@@ -17,17 +25,27 @@ public class MainActivity extends FlutterActivity {
             @Override
             public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
                 final Map<String, Object> arguments = methodCall.arguments();
-                if (methodCall.method.equals("getMessage")) {
-                   openWebPage("https://flutter.io");
+                if (methodCall.method.equals("openFile")) {
+                   openFile((String) arguments.get("filePath"));
                 }
             }
       });
+  }
 
-    //    var sendMap = <String, dynamic>{
-    //   'filePath': filePath,
-    // };
+  private void openFile(String filePath){
+      File file = new File(filePath);
+      MimeTypeMap map = MimeTypeMap.getSingleton();
+      String ext = MimeTypeMap.getFileExtensionFromUrl(file.getName());
+      String type = map.getMimeTypeFromExtension(ext);
 
-    // try {
-    //   await platform.invokeMethod('openFile', sendMap);
+      if (type == null)
+          type = "*/*";
+
+      Intent intent = new Intent(Intent.ACTION_VIEW);
+      Uri data = Uri.fromFile(file);
+
+      intent.setDataAndType(data, type);
+
+      startActivity(intent);
   }
 }
