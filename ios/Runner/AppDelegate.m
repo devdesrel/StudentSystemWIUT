30597d161fcfe1d9f0df0a118ead2b843a05d1a8
Administrator <administrator@Administrators-iMac-Pro.local>
2018-08-29 18:58:21 +0500

    delegate changes

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -4,6 +4,7 @@
 @implementation AppDelegate
 
 
+//- (UIViewController *)documentInteractionControllerViewControllerForPreview:(UIDocumentInteractionController *)controller2;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
@@ -17,10 +18,15 @@
 
     [channel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
         NSString *filePath = call.arguments[@"filePath"];
-        NSString *mimeType = call.arguments[@"mimeType"];
+//        NSString *mimeType = call.arguments[@"mimeType"];
         
         if([@"openFile" isEqualToString:call.method]){
-          
+//            NSURL *URL = [[NSBundle mainBundle] URLForResource:@"Your PDF Name" withExtension:@"pdf"];
+            NSURL *url = [NSURL fileURLWithPath:[filePath stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]]];
+            
+            UIDocumentInteractionController *documentInteractionController =[UIDocumentInteractionController interactionControllerWithURL:url];
+              documentInteractionController.delegate = self.window.rootViewController;
+            [documentInteractionController presentPreviewAnimated:YES];
         }
     }];
     
