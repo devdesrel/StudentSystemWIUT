041ef30351a0f689e8fdf77c64d920cf196be1fc
Ramziddin Toshmukhamedov <rtoshmukhamedov@gmail.com>
2018-09-05 01:02:00 +0500

    pull fixes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,7 +5,9 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-    
+  
+  var flutterChannelManager : FlutterChannelManager!
+  
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
@@ -16,33 +18,14 @@ import Flutter
     
     
     let controller : FlutterViewController = window?.rootViewController as! FlutterViewController;
-    let fileOpenerChannel = FlutterMethodChannel.init(name: "com.rtoshmukhamedov.flutter.fileopener",
-                                                   binaryMessenger: controller);
-    fileOpenerChannel.setMethodCallHandler({
-        (call: FlutterMethodCall, result: FlutterResult) -> Void in
-        let filePath = call.arguments as! String;
-        //        NSString *mimeType = call.arguments[@"mimeType"];
+   
 
-        if ("openFile" == call.method) {
-//
-        } else {
-            result(FlutterMethodNotImplemented);
-        }
-        
-    });
+
+    flutterChannelManager = FlutterChannelManager(flutterViewController: controller)
+    flutterChannelManager.setup()
+
+
     
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-    
-    private func receiveBatteryLevel(result: FlutterResult) {
-        let device = UIDevice.current;
-        device.isBatteryMonitoringEnabled = true;
-        if (device.batteryState == UIDeviceBatteryState.unknown) {
-            result(FlutterError.init(code: "UNAVAILABLE",
-                                     message: "Battery info unavailable",
-                                     details: nil));
-        } else {
-            result(Int(device.batteryLevel * 100));
-        }
-    }
 }
